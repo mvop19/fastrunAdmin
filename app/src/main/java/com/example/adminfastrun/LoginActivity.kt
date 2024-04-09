774296c9520197dfe6a961d9a02a856c35bfd04d
zapatam671 <mattzapata671@gmail.com>
2024-04-10 01:16:22 +1000

    finish login and signup

--- app/src/main/java/com/example/adminfastrun/LoginActivity.kt
+++ app/src/main/java/com/example/adminfastrun/LoginActivity.kt
@@ -1,20 +1,28 @@
 package com.example.adminfastrun
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfastrun.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.dontHaveAccountButton.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/adminfastrun/SignUpActivity.kt
+++ app/src/main/java/com/example/adminfastrun/SignUpActivity.kt
@@ -1,20 +1,35 @@
 package com.example.adminfastrun
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfastrun.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private val binding: ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.createButton.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
+        binding.alreadyHaveAccountButton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val locationList = arrayOf("Yigo", "Dededo", "Mangilao", "Tumon")
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listofLocation
+        autoCompleteTextView.setAdapter(adapter)
+
     }
 }
