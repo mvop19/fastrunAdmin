4192d4250a236c4f0ede77b9bb3dd7cb7bc00472
zapatam671 <mattzapata671@gmail.com>
2024-04-12 17:22:43 +1000

    start on order status item (out for delivery)

---
+++ app/src/main/java/com/example/adminfastrun/OrderStatusActivity.kt
@@ -0,0 +1,16 @@
+package com.example.adminfastrun
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order_status)
+
+    }
+}
