6c1a3f12fb1ef4ec5e11181340c0163641ffbca4
zapatam671 <mattzapata671@gmail.com>
2024-04-14 18:01:58 +1000

    save progress on login and signup, need to do google login

--- app/src/main/java/com/example/adminfastrun/LoginActivity.kt
+++ app/src/main/java/com/example/adminfastrun/LoginActivity.kt
@@ -2,13 +2,29 @@ package com.example.adminfastrun
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminfastrun.databinding.ActivityLoginBinding
+import com.example.adminfastrun.model.UserModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class LoginActivity : AppCompatActivity() {
+    private  var userName: String ?= null
+    private  var nameOfRestaurant: String ?= null
+    private lateinit var email:String
+    private lateinit var password:String
+    private lateinit var auth:FirebaseAuth
+    private lateinit var database:DatabaseReference
+
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -16,13 +32,67 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        //initialize Firebase Auth
+        auth = Firebase.auth
+        //initialize Firebase database
+        database = Firebase.database.reference
+
         binding.loginButton.setOnClickListener {
-            val intent = Intent(this,SignUpActivity::class.java)
-            startActivity(intent)
+            // get text from edit text
+            email = binding.email.text.toString().trim()
+            password = binding.password.text.toString().trim()
+
+            if (email.isBlank() || password.isBlank()){
+                Toast.makeText(this, "Please Fill All Details", Toast.LENGTH_SHORT).show()
+            } else {
+                createUserAccount(email, password)
+            }
+
         }
         binding.dontHaveAccountButton.setOnClickListener {
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun createUserAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                updateUi(user)
+            } else{
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        val user= auth.currentUser
+                        Toast.makeText(this, "Create User & Login Successful", Toast.LENGTH_SHORT).show()
+                        saveUserData()
+                        updateUi(user)
+                    } else{
+                        Toast.makeText(this,"Authentication Failed", Toast.LENGTH_SHORT).show()
+                        Log.d("Account", "createUserAccount: Authentication failed", task.exception)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveUserData() {
+        // get text from edit text
+
+        email = binding.email.text.toString().trim()
+        password = binding.password.text.toString().trim()
+
+        val user = UserModel(userName,nameOfRestaurant,email, password)
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        userId?.let {
+            database.child("user").child(it).setValue(user)
+        }
+    }
+
+    private fun updateUi(user: FirebaseUser?) {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
