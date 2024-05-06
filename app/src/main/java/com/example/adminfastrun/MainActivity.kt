616a959a8c80c34ccab1405ed062ee8d4b1af23e
zapatam671 <mattzapata671@gmail.com>
2024-05-06 13:03:23 +1000

    save progress on pending and completed orders

--- app/src/main/java/com/example/adminfastrun/MainActivity.kt
+++ app/src/main/java/com/example/adminfastrun/MainActivity.kt
@@ -5,11 +5,21 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.adminfastrun.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var completedOrderReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,5 +48,42 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,PendingOrderActivity::class.java)
             startActivity(intent)
         }
+
+        pendingOrders()
+
+        completedOrders()
+    }
+
+    private fun completedOrders() {
+        val completedOrderReference = database.reference.child("CompletedOrder")
+        var completedOrderItemCount = 0
+        completedOrderReference.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                completedOrderItemCount = snapshot.childrenCount.toInt()
+                binding.completeOrders.text = completedOrderItemCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun pendingOrders() {
+        database = FirebaseDatabase.getInstance()
+        val pendingOrderReference = database.reference.child("OrderDetails")
+        var pendingOrderItemCount = 0
+        pendingOrderReference.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pendingOrderItemCount = snapshot.childrenCount.toInt()
+                binding.pendingOrders.text = pendingOrderItemCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
